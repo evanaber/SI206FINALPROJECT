8e64f07268dc85f29d84b28ada6664096880f077
Eva <evanaber@umich.com>
2024-12-02 14:42:28 -0500

    started API

--- finalproject.py
+++ finalproject.py
@@ -1,1 +1,20 @@
-#finalproject ##
+import requests
+import json
+import matplotlib
+
+uri = 'https://api.football-data.org/v4/competitions'
+headers = { 'X-Auth-Token': 'c13b0efbe1df4ac68a950a03595a03c0' }
+
+response = requests.get(uri, headers=headers)
+competitions = response.json().get('competitions', [])
+for competition in competitions:
+    if competition['name'] == 'Premier League':
+        pleague_id = competition['id']
+print (pleague_id)
+'''
+for match in response.json()['matches']:
+    print({match['id']})
+    print(json.dumps(match, indent=4))
+'''
+#get match
+#get date and score from match
