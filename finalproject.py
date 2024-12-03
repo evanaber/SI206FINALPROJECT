89f9219911bebbc8b575d18846408d796cf3400d
Eva <evanaber@umich.com>
2024-12-02 21:59:03 -0500

    started making SQL databases

--- finalproject.py
+++ finalproject.py
@@ -27,17 +27,56 @@ urid = f'https://api.football-data.org/v4/matches?competitions={pleague_id}&date
 
 response = requests.get(urid, headers=headers)
 responses = response.json()
-#print(json.dumps(responses, indent=4))
+
+
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS Date_Keys (id INTEGER PRIMARY KEY, date TEXT)"
+)
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS Scores (date INTEGER, t_score INTEGER)"
+)
 
 for match in responses['matches']:
-    #print(json.dumps(match, indent=4))
+    #gets date
     date_pattern = r"\d{4}-\d{2}-\d{2}"
     date = match["utcDate"]
     date = re.search(date_pattern, date)
     date = date.group()
-    homeScore = match['score']['fullTime']['home']
-    awayScore = match['score']['fullTime']['away']
-    total_score = int(homeScore) + int(awayScore)
+    #gets scores of home and away teams
+    homeScore = int(match['score']['fullTime']['home'])
+    awayScore = int(match['score']['fullTime']['away'])
+    #gets total goals scored
+    total_score = homeScore + awayScore
+    #sees if date has already been accessed
+    cur.execute('SELECT id from Date_Keys WHERE date = ?', (date,))
+    #if not
+    if cur.fetchone() is None:
+        #add date to table
+        cur.execute(
+            "INSERT INTO Date_Keys (date) VALUES (?)",
+            (date,)
+        )
+        cur.execute('SELECT id FROM Date_Keys WHERE date = ?', (date,))
+        date_id = cur.fetchone()[0]
+        print(f"Inserted date: {date}, id: {date_id}")  # Debugging line
+        #start adding to scores table
+        cur.execute(
+            "INSERT INTO Scores (date, t_score) VALUES (?,?)",
+            (date_id, total_score)
+        )
+        conn.commit()
+    else:
+        #update the total goals scored for that day
+        print(f"Date already exists: {date}")  # Debugging line
+
+        pass
+    conn.commit()
+
+    
+#how should I limit to 25?
+
+
+
 
 #get match
 #get date and score from match
