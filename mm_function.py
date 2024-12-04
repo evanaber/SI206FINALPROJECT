bc5cc1ee36c6edf93424c76325eae2ec683eb9b0
mmhatch <mmhatch@umich.edu>
2024-12-03 22:28:40 -0500

    done with api call

--- mm_function.py
+++ mm_function.py
@@ -11,16 +11,51 @@ def set_up_db(db_name):
     cur = conn.cursor()
     return cur, conn
 
-def weather_api(dates, place):
-    weather_api_key = f"2212ddc784060812ce08fdf3b5b692ec"
-    semi = ";"
-    api_date = semi.join(dates)
-    url = "https://api.weatherstack.com/historical?access_key={weather_api_key}"
-    querystring = {f"query":{place}, "historical_date":{api_date}}
+def weather_api(date, latitude, longitude, cur, conn):
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    querystring = {"latitude": latitude, "longitude": longitude, "start_date": date, "end_date": date, "daily": ["temperature_2m_mean", "precipitation_sum"], "temperature_unit": "fahrenheit"}
     response = requests.get(url, params=querystring)
-    print(response.json())
-    for date in dates:
-        weather_data = response["historical"][date]
-        avgtemp = weather_data["avgtemp"]
-        uv_index = weather_data["uv_index"]
-    
+    response = response.json()
+    print(f'{response}')
+    elevation = response['elevation']
+    temperature = response['daily']['temperature_2m_mean'][0]
+    precipitation = response['daily']['precipitation_sum'][0]
+    cur.execute(
+    "CREATE TABLE IF NOT EXISTS Date_Keys (id INTEGER PRIMARY KEY, date TEXT)"
+)
+    cur.execute(
+    "CREATE TABLE IF NOT EXISTS Weather (date_id INTEGER, latitude FLOAT, longitude FLOAT, elevation FLOAT, temperature FLOAT, precipitation FLOAT)"
+)
+    cur.execute('SELECT id from Date_Keys WHERE date = ?', (date,))
+    #if not
+    if cur.fetchone() is None:
+        #add date to table
+        cur.execute(
+            "INSERT INTO Date_Keys (date) VALUES (?)",
+            (date,)
+        )
+        cur.execute('SELECT id FROM Date_Keys WHERE date = ?', (date,))
+        date_id = cur.fetchone()[0]
+        print(f"Inserted date: {date}, id: {date_id}")  # Debugging line
+        #start adding to scores table
+    else:
+        #update the total goals scored for that day
+        print(f"Date already exists: {date}") 
+        cur.execute('SELECT id FROM Date_Keys WHERE date = ?', (date,))
+        date_id = cur.fetchone()[0] # Debugging line
+        
+
+
+    cur.execute(
+            "INSERT INTO Weather (date_id, latitude, longitude, elevation, temperature, precipitation) VALUES (?,?,?,?,?,?)",
+            (date_id, latitude, longitude, elevation, temperature, precipitation)
+        )
+    conn.commit()
+
+#h= set_up_db('football_weather')
+cur, conn = set_up_db("football_weather.db")
+#weather_api('2024-11-15', 42.27, -83.73,cur, conn) 
+#weather_api('2024-11-15', 50, -99,cur, conn) 
+#not sure if duplicate data will be a problem 
+#we take out some of the code when combined 
+ 
