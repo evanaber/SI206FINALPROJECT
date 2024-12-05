d3d17908a2b80ba903e123a3ad6d55f37394e558
Eva <evanaber@umich.com>
2024-12-05 13:07:28 -0500

    updating database

--- finalproject.py
+++ finalproject.py
@@ -29,31 +29,53 @@ for competition in competitions:
 #urid = f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-10-23&dateTo=2024-11-01'
 #urid = f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-10-13&dateTo=2024-10-22'
 #urid = f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-10-03&dateTo=2024-10-12'
-urid = f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-11-25&dateTo=2024-12-04'
 
 #If we don't have enough dates, we could do singular matches and keep track of scores that way. Idk how far back
 #the data from the API goes.
 
-response = requests.get(urid, headers=headers)
-responses = response.json()
+def set_api(link):
+    urid = link
+    response = requests.get(urid, headers=headers)
+    responses = response.json()
+    return responses
 
+#are we not supposed to use these?
+cur.execute(
+    'DELETE FROM Date_Keys'
+)
+cur.execute(
+    'DELETE FROM Scores'
+)
 cur.execute(
     "CREATE TABLE IF NOT EXISTS Date_Keys (id INTEGER PRIMARY KEY, date TEXT)"
 )
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS Scores (date INTEGER, t_score INTEGER)"
+    "CREATE TABLE IF NOT EXISTS Scores (game_num INTEGER PRIMARY KEY, date INTEGER, t_score INTEGER)"
 )
 #total score keeps getting too high because we are running the same dates over and over again. 
 #I need to access new ones and reset the database each time we run the code from the beginning
-
+def test_dif_dates():
+    responses = set_api(f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-11-23&dateTo=2024-12-01')
+    find_matches(responses)
+    responses = set_api(f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-11-13&dateTo=2024-11-23')
+    find_matches(responses)
+    responses = set_api(f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-10-23&dateTo=2024-11-01')
+    find_matches(responses)
+    responses = set_api(f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-10-13&dateTo=2024-10-22')
+    find_matches(responses)
+    responses = set_api(f'https://api.football-data.org/v4/matches?competitions={pleague_id}&dateFrom=2024-10-03&dateTo=2024-10-12')
+    find_matches(responses)
 # call it with different dates
-def find_matches():
+def find_matches(responses):
     for match in responses['matches']:
     #gets date
         date_pattern = r"\d{4}-\d{2}-\d{2}"
         date = match["utcDate"]
         date = re.search(date_pattern, date)
         date = date.group()
+    #gets name of away team for location data
+        homeTeam = match['homeTeam']['name']
+        venue = team_loc[homeTeam]
     #gets scores of home and away teams
         homeScore = int(match['score']['fullTime']['home'])
         awayScore = int(match['score']['fullTime']['away'])
@@ -62,13 +84,20 @@ def find_matches():
     #sees if date has already been accessed
         cur.execute('SELECT id from Date_Keys WHERE date = ?', (date,))
     #if not
-    #dates are not updating for some reason
         if cur.fetchone() is None:
         #add date to table
             cur.execute(
                 "INSERT INTO Date_Keys (date) VALUES (?)",
                 (date,)
+            )            
+        cur.execute('SELECT id FROM Date_Keys WHERE date = ?', (date,))
+        date_id = cur.fetchone()[0]
+        cur.execute(
+                "INSERT INTO Scores (date, t_score) VALUES (?,?)",
+                (date_id, total_score)
             )
+        '''
+        if 0 = 0:
             cur.execute('SELECT id FROM Date_Keys WHERE date = ?', (date,))
             date_id = cur.fetchone()[0]
         #start adding to scores table
@@ -85,36 +114,41 @@ def find_matches():
             updated_score = int(c_score) + total_score
             cur.execute("UPDATE Scores SET t_score = ? WHERE date = ?",(updated_score, dateid))
         #update the total goals scored for that day
+        '''
+
         conn.commit()
     
 #make dictionary to set home team equal to location (city name)
 team_loc = {
-    'Arsenal':'London',
-    'Aston Villa':'Birmingham',
-    'Bournemouth':'Bournemouth',
-    'Brentford':'London',
-    'Brighton & Hove Albion':'Brighton',
-    'Chelsea':'London',
-    'Crystal Palace':'London',
-    'Everton':'Liverpool',
-    'Fulham':'London',
-    'Ipswich Town':'Ipswich',
-    'Leicester City':'Leicester',
-    'Liverpool':'Liverpool',
-    'Manchester City':'Manchester',
-    'Manchester United':'Trafford',
-    'Newcastle United':'Newcastle upon Tyne',
-    'Nottingham Forest':'West Bridgford',
-    'Southampton':'Southampton',
-    'Tottenham Hotspur':'London',
-    'West Ham United':'London',
-    'Wolverhampton Wanderers':'Wolverhampton',
+    'Arsenal FC':'London',
+    'Aston Villa FC':'Birmingham',
+    'AFC Bournemouth':'Bournemouth',
+    'Brentford FC':'London',
+    'Brighton & Hove Albion FC':'Brighton',
+    'Chelsea FC':'London',
+    'Crystal Palace FC':'London',
+    'Everton FC':'Liverpool',
+    'Fulham FC':'London',
+    'Ipswich Town FC':'Ipswich',
+    'Leicester City FC':'Leicester',
+    'Liverpool FC':'Liverpool',
+    'Manchester City FC':'Manchester',
+    'Manchester United FC':'Trafford',
+    'Newcastle United FC':'Newcastle upon Tyne',
+    'Nottingham Forest FC':'West Bridgford',
+    'Southampton FC':'Southampton',
+    'Tottenham Hotspur FC':'London',
+    'West Ham United FC':'London',
+    'Wolverhampton Wanderers FC':'Wolverhampton',
 }
     
 #how should I limit to 25?
 
 def main():
-    find_matches()
+   test_dif_dates()
+
+
+
 
 
 if __name__ == "__main__":
